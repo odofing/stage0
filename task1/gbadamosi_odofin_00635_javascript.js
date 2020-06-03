c74e4add714d554a7d9fb15fd8d26e66669c2127
odofing <odofing@gmail.com>
2020-06-03 13:02:29 +0100

    On branch odofing

---
+++ task1/gbadamosi_odofin_00635_javascript.js
@@ -0,0 +1,5 @@
+const project = ["Odofin Gbadamosi", "00635", "Javascript"];
+[arr1, arr2, arr3] = project;
+
+ 
+ console.log(`Hello World, this is ${arr1} with ${arr2} using ${arr3} for stage 2 task`);
