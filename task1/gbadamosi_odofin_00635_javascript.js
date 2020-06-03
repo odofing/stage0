634775b82263395b90aceceb36cc8a257fe47b99
odofing <odofing@gmail.com>
2020-06-03 13:23:29 +0100

    On branch odofing  Changes to be committed: 	modified:   task1/gbadamosi_odofin_00635_javascript.js

--- task1/gbadamosi_odofin_00635_javascript.js
+++ task1/gbadamosi_odofin_00635_javascript.js
@@ -1,5 +1,5 @@
-const project = ["Odofin Gbadamosi", "00635", "Javascript"];
-[arr1, arr2, arr3] = project;
+const project = ["Odofin Gbadamosi", "00635", "Javascript", "odofing@gmail.com"];
+[arr1, arr2, arr3, arr4] = project;
 
  
- console.log(`Hello World, this is ${arr1} with ${arr2} using ${arr3} for stage 2 task`);
+ console.log(`Hello World, this is ${arr1} with ${arr2} with email ${arr4} using ${arr3} for stage 2 task`);
